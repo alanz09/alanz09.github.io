95768a073891b9e36501bcca9b58eb0573195b6a
Amy Lanza <alanz09@gmail.com>
2017-04-21 12:22:45 -0400

    new

--- finalproj/harmon.js
+++ finalproj/harmon.js
@@ -33,7 +33,7 @@ var valueline = d3.svg.line()
     .interpolate('cardinal');
     
 // Adds the svg canvas
-var svg = d3.select("#gibsonchart")
+var svg = d3.select("#harmonchart")
     .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -41,46 +41,19 @@ var svg = d3.select("#gibsonchart")
         .attr("transform", 
               "translate(" + margin.left + "," + margin.top + ")");
 // Get the data
-data = [{movie:"The Expendables 3",year:2014,rating:32,box_office:39322544},
-          {movie:"Machete Kills",year:2013,rating:28,box_office:8008161},
-          {movie:"Get the Gringo",year:2012,rating:81,box_office:5793167},
-          {movie:"The Beaver",year:2011,rating:61,box_office:970816},
-          {movie:"Edge of Darkness",year:2010,rating:56,box_office:43313890},
-          {movie:"Paparazzi",year:2004,rating:18,box_office:15714234},
-          {movie:"The Singing Detective",year:2003,rating:39,box_office:337174},
-          {movie:"Signs",year:2002,rating:74,box_office:227966634},
-          {movie:"We Were Soldiers",year:2002,rating:63,box_office:78122718},
-          {movie:"What Woman Want",year:2000,rating:54,box_office:182811707},
-          {movie:"The Patriot",year:2000,rating:61,box_office:113330342},
-          {movie:"Chicken Run",year:2000,rating:97,box_office:106834564},
-          {movie:"The Million Dollar Hotel",year:2000,rating:26,box_office:59989},
-          {movie:"Payback",year:1999,rating:54,box_office:81526121},
-          {movie:"Lethal Weapon 4",year:1998,rating:52,box_office:130444603},
-          {movie:"Fairytale: A True Story",year:1997,rating:50,box_office:14059077},
-          {movie:"Conspiracy Theory",year:1997,rating:52,box_office:75982834},
-          {movie:"Father's Day",year:1997,rating:25,box_office:28598376},
-          {movie:"Ransom",year:1996,rating:75,box_office:136492681},
-          {movie:"Pocahontas",year:1995,rating:56,box_office:141579773},
-          {movie:"Casper",year:1995,rating:44,box_office:100328194},
-          {movie:"Braveheart",year:1995,rating:78,box_office:75609945},
-          {movie:"Maverick",year:1994,rating:67,box_office:101631272},
-          {movie:"The Man Without a Face",year:1993,rating:67,box_office:24760338},
-          {movie:"Forever Young",year:1992,rating:53,box_office:55956187},
-          {movie:"Lethal Weapon 3",year:1992,rating:56,box_office:144731527},
-          {movie:"Hamlet",year:1990,rating:76,box_office:20710451},
-          {movie:"Air America",year:1990,rating:13,box_office:31053601},
-          {movie:"Bird on a Wire",year:1990,rating:32,box_office:70978012},
-          {movie:"Lethal Weapon 2",year:1989,rating:83,box_office:147253986},
-          {movie:"Tequila Sunrise",year:1988,rating:42,box_office:41292551},
-          {movie:"Lethal Weapon",year:1987,rating:83,box_office:65207127},
-          {movie:"Mad Max Beyond Thunderdome",year:1985,rating:81,box_office:36230219},
-          {movie:"Mrs. Soffel",year:1984,rating:40,box_office:4385312},
-          {movie:"The Bounty",year:1984,rating:82,box_office:8613462},
-          {movie:"The Year of Living Dangerously",year:1982,rating:89,box_office:10278575},
-          {movie:"Mad Max 2: The Road Warrior",year:1982,rating:98,box_office:23667907},
-          {movie:"Gallipoli",year:1981,rating:88,box_office:5732587},
-          {movie:"Mad Max",year:1979,rating:90,box_office:8750000}
-    ];
+data = [{movie:"Chasing Liberty",year:2004,rating:19,box_office:12195626},
+        {movie:"Freaky Friday",year:2003,rating:88,box_office:110230332},
+        {movie:"The Amati Girls",year:2000,rating:46,box_office:49136},
+        {movie:"Fear and Loathing in Las Vegas",year:1998,rating:49,box_office:10680275},
+        {movie:"The Last Supper",year:1995,rating:63,box_office:459749},
+        {movie:"Magic in the Water",year:1995,rating:21,box_office:2680717},
+        {movie:"Natural Born Killers",year:1994,rating:46,box_office:50282766},
+        {movie:"Wyatt Earp",year:1994,rating:42,box_office:25052000},
+        {movie:"Stealing Home",year:1988,rating:22,box_office:7467504},
+        {movie:"The Presidio",year:1988,rating:53,box_office:20324096},
+        {movie:"Summer School",year:1987,rating:61,box_office:35659098},
+        {movie:"Comes a Horseman",year:1978,rating:86,box_office:9585769}
+       ];
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.year; }));
